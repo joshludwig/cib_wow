cdfd2b57cf99c1e099a1e7b90ec720c0c8ad0c56
Joshua Ludwig <joshualudwig@Joshuas-MBP.home>
2015-04-10 22:55:44 -0400

    Removed puts

--- lib/update_feed.rb
+++ lib/update_feed.rb
@@ -9,14 +9,10 @@ loot_thread = Thread.new {
   last_check = DateTime.now + (1/24.0)
 
   while true
-    puts 'Get items'
     items = WowLootFeed::get_new_items last_check
-    puts 'Got items'
 # update slack
     payloads = SlackWebHook::create_payloads(items)
-    puts 'Send to slack'
     SlackWebHook::update_slack(payloads)
-    puts 'Sent to slack'
     last_check = DateTime.now + (1/24.0)
     sleep 15
   end
