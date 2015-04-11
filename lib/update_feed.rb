be3fae236349448a712a169b97c0e397740bbde9
Joshua Ludwig <joshualudwig@Joshuas-MBP.home>
2015-04-10 22:59:01 -0400

    changed going live to be the metal format

--- lib/update_feed.rb
+++ lib/update_feed.rb
@@ -2,7 +2,7 @@
 # timestamp for debugging
 # last_check = Time.at(1428701100000/1000).to_datetime
 
-live = '{"text": "Loot feed is live"}'
+live = '{"text": "Loot feed is live","username": "loot-bot", "icon_emoji": ":metal:"}'
 RestClient.post "https://hooks.slack.com/services/#{ENV['SLACK_API_KEY']}", live
 
 loot_thread = Thread.new {
