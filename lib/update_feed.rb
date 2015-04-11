688df404b3f60e0c9dded75ddf6cb0eb91167b42
Joshua Ludwig <joshualudwig@Joshuas-MBP.home>
2015-04-10 22:27:02 -0400

    Added init message to send to slack so I know the heroku app is live

--- lib/update_feed.rb
+++ lib/update_feed.rb
@@ -2,6 +2,9 @@
 # timestamp for debugging
 # last_check = Time.at(1428701100000/1000).to_datetime
 
+live = '{"text": "Loot feed is live"}'
+RestClient.post "https://hooks.slack.com/services/#{ENV['SLACK_API_KEY']}", live
+
 loot_thread = Thread.new {
   last_check = DateTime.now + (1/24.0)
 
